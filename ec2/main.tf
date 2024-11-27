315f1d291903d74372fb0f88eff7ef60625cabab
Shan2k <144056109+Shantanukharate@users.noreply.github.com>
2024-11-27 23:58:10 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
     ami = var.ami_id
     instance_type = var.instance_type
-    instance_count = var.instance_count
+    count = var.instance_count
 
-    subnet_id = element{aws_subnet.public[*].id, count.index}
-    assosiate_punlic_ip_address = true 
+    subnet_id = element(aws_subnet.public[*].id, count.index)
+    assosiate_public_ip_address = true 
 
     tags ={
         Name = "web-instance-${count.index + 1}"
