ec08eb66409484d1e12d2377444f1c3a432212fd
Shan2k <144056109+Shantanukharate@users.noreply.github.com>
2024-11-27 23:50:45 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
     instance_type = var.instance_type
     instance_count = var.instance_count
 
-    subnet_id = element{aws_subnet.public.*.id,count.index}
+    subnet_id = element{aws_subnet.public[*].id, count.index}
     assosiate_punlic_ip_address = true 
 
     tags ={
