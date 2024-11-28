d053e1d65619baa425127e9b6ae931e9a79c2f86
Shan2k <144056109+Shantanukharate@users.noreply.github.com>
2024-11-28 12:12:02 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -6,7 +6,7 @@ variable "public_subnets_cidr" {
     default = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
 
-variable "private_subnets-cidr" {
+variable "private_subnets_cidr" {
     default = ["10.0.3.0/24" , "10.0.4.0/24"]
 }
 
