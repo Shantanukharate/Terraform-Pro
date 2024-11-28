955db670f313db0792095c9b7deb55c8fd856115
Shan2k <144056109+Shantanukharate@users.noreply.github.com>
2024-11-28 12:02:19 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,11 +9,11 @@ resource "aws_vpc" "main"{
 resource "aws_subnet" "public" {
     count = 2
     vpc_id = aws_vpc.main.id
-    cidr_block = element(var.public_subnets_cidr,count.index) 
+    cidr_block = element(var.public_subnets_cidr, count.index) 
     map_public_ip_on_launch = true
     availability_zone = element(var.availability_zones,count.index)
     tags = {
-        Name = "public-subnet-${count.index +1 }"
+        Name = "public Subnet ${count.index +1 }"
     }
 }
 
